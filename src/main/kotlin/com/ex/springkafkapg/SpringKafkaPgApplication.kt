bc10f8178f019e57978073fffbadea7960320347
Park Sung Jun <junstin119@gmail.com>
2025-04-27 01:20:49 +0900

    프로젝트 설정
    - 도커 컴포즈 파일 생성

---
+++ src/main/kotlin/com/ex/springkafkapg/SpringKafkaPgApplication.kt
@@ -0,0 +1,11 @@
+package com.ex.springkafkapg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKafkaPgApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKafkaPgApplication>(*args)
+}
